ef5c16d48e0b8a6a6acc8e533e9bb94c760be888
John Gardner <john.gardner@msn.com>
2024-11-14 19:19:52 +0000

    add ev

--- codestar/settings.py
+++ codestar/settings.py
@@ -15,6 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&(zwz=gx4_jjc=*j6i_pqvz635)e3=l4&d2xs#h#^(^k5etq$'
+# SECRET_KEY = 'django-insecure-e&(zwz=gx4_jjc=*j6i_pqvz635)e3=l4&d2xs#h#^(^k5etq$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
