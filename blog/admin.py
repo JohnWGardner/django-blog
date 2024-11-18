03627d54ee4151f0e57766ebb2657cbf2f3d6583
John Gardner <john.gardner@msn.com>
2024-11-18 13:15:20 +0000

    Admin panel upgrade

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register() 
+@admin.register(Post) 
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status')
@@ -13,5 +13,5 @@ class PostAdmin(SummernoteModelAdmin):
 
 # Register your models here.
 
-# admin.site.register(Post)
+# admin.site.register(Post)  - Now we have a decorator above the PostAdmin class; delete the existing Post model registration.
 admin.site.register(Comment)
